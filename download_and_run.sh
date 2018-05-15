aa86cb02fd95404fdfba8cf9d13cb5c18138b6b5
Martin Ashby <martin@martin-laptop.lan>
2018-05-15 22:47:34 +0100

    urls

--- download_and_run.sh
+++ download_and_run.sh
@@ -1,4 +1,4 @@
 pip3 install unicornhathd Flask Flask-Sockets \
-&& wget -N "https://raw.githubusercontent.com/MFAshby/unicornpaint/master/download_and_run.sh" \
+&& wget -N "https://raw.githubusercontent.com/MFAshby/unicornpaint/master/unicornpaint.zip" \
 && unzip unicornpaint.zip \
 && python3 server.py
