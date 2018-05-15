76e90d19b50303f915c1ce45b53fd51fb9590057
Martin Ashby <martin@martin-laptop.lan>
2018-05-15 22:41:47 +0100

    Set correct url

--- download_and_run.sh
+++ download_and_run.sh
@@ -1,4 +1,4 @@
 pip3 install unicornhathd Flask FlaskSockets \
-&& wget -N "https://github.com/MFAshby/unicornpaint/raw/master/unicornpaint.zip" \
+&& wget -N "https://raw.githubusercontent.com/MFAshby/unicornpaint/master/download_and_run.sh" \
 && unzip unicornpaint.zip \
 && python3 server.py
